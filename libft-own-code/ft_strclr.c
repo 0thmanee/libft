53f9d9c0fe7e2f4f29a017ea427325fe518915e5
0thmanee <bouchtaothmane75@gmail.com>
2023-10-20 20:39:58 +0100

    .

---
+++ libft-own-code/ft_strclr.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	ft_strclr(char *s)
+{
+    int i;
+
+	if (s)
+    {
+        i = 0;
+        while (s[i])
+        {
+            s[i] = 0;
+            i++;
+        }
+    }
+}
