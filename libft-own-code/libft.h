1a8cbc41fa63fdd57f380a367623b6bb25b56ab4
0thmanee <bouchtaothmane75@gmail.com>
2023-10-20 20:34:13 +0100

    .

--- libft-own-code/libft.h
+++ libft-own-code/libft.h
@@ -30,4 +30,6 @@ int	    ft_isprint(int c);
 int	    ft_toupper(int c);
 int	    ft_tolower(int c);
 void    *ft_memalloc(size_t size);
+void    ft_memdel(void **ap);
+char    *ft_strnew(size_t size);
 #endif
