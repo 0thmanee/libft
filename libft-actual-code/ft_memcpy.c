4f186d811c3f0aa56b3c5c353196927919ce759a
0thmanee <bouchtaothmane75@gmail.com>
2023-11-01 17:37:05 +0100

    .

---
+++ libft-actual-code/ft_memcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouchta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 15:12:00 by obouchta          #+#    #+#             */
+/*   Updated: 2023/11/01 15:12:03 by obouchta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t  i;
+	char	*src2;
+	char	*dest2;
+
+	if (src == NULL && dest == NULL)
+		return (0);
+	i = 0;
+	src2 = (char*)src;
+	dest2 = (char*)dest;
+	while (i < n)
+	{
+		dest2[i] = src2[i];
+		i++;
+	}
+	return (dest);
+}
+
+/*Why we check only if one is NULL*/
+
---
+++ libft-actual-code/ft_memmove.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouchta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 15:44:31 by obouchta          #+#    #+#             */
+/*   Updated: 2023/11/01 15:44:35 by obouchta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+#include<string.h>
+#include<stdio.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t len)
+{
+	size_t	i;
+	char	*src2;
+	char	*dest2;
+
+	src2 = (char*)src;
+	dest2 = (char*)dest;
+	if (dest > src)
+	{
+		while (len > 0)
+		{
+			dest2[len - 1] = src2[len - 1];
+			len--;
+		}
+	}
+	else
+		memcpy(dest, src, len);
+	return (dest);
+}
