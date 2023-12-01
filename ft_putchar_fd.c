c3399c04fe0a80366e3714f79cf3c78d678ee943
0thmanee <bouchtaothmane75@gmail.com>
2023-12-01 22:52:17 +0100

    last

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: obouchta <obouchta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 19:17:26 by obouchta          #+#    #+#             */
-/*   Updated: 2023/11/14 13:50:04 by obouchta         ###   ########.fr       */
+/*   Updated: 2023/11/16 12:59:26 by obouchta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,4 @@ void	ft_putchar_fd(char c, int fd)
 	if (fd == -1)
 		return ;
 	write (fd, &c, 1);
-}
-
-#include <fcntl.h>
-int main()
-{
-	int fd = open("test", O_RDONLY);
-	sleep(100);
 }
